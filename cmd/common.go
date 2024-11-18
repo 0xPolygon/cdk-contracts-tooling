6443c2aa5d8acf24894b18f7b468ea5a625c6919
Stefan Negovanović <stefan@ethernal.tech>
2024-11-18 15:37:05 +0100

    Introduce runCommand helper function

--- cmd/common.go
+++ cmd/common.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 	"path"
+	"strings"
 )
 
 const (
@@ -20,4 +23,22 @@ func checkWorkingDir() (string, error) {
 		return "", fmt.Errorf("run the command from the root of the (%s), not from (%s)", repoName, baseDir)
 	}
 	return baseDir, nil
+}
+
+// runCommand executes given command and extracts output from standard error in case of failure.
+// Also in case command is executed successfully, the standard output is printed to the terminal.
+func runCommand(cmdName string, args ...string) error {
+	var stdout, stderr bytes.Buffer
+
+	cmd := exec.Command(cmdName, args...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("%s %s", err, stderr.String())
+	}
+
+	cmdAndArgs := cmdName + " " + strings.Join(args, " ")
+	fmt.Printf("command '%s' executed successfully\n%s", cmdAndArgs, stdout.String())
+
+	return nil
 }
