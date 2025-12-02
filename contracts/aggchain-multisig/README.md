551dbaf490d5f14e1d8d6184353143fea9943b64
Stefan Negovanović <stefan@ethernal.tech>
2025-12-02 08:44:43 +0100

    use the v12.2.0 contracts ref to generate bindings

--- contracts/aggchain-multisig/README.md
+++ contracts/aggchain-multisig/README.md
@@ -1,6 +1,6 @@
-# aggchain-multisig contracts
+# aggchain-multisig-new contracts
 
 All the files and directories within this directory have been generated using the import-contracts command of the CLI in this repo.
-The ABI and the binnaries of the smart contracts have been extracted from [agglayer-contracts repo](https://github.com/agglayer/agglayer-contracts.git), using the version v12.1.0 (commit c01773e73f62abdeaee36b29c1c20b99aa05abf3)
+The ABI and the binnaries of the smart contracts have been extracted from [agglayer-contracts repo](https://github.com/agglayer/agglayer-contracts.git), using the version v12.2.0 (commit f27cc65f4b2ed5dcd4342c6d8f903738ac439418)
 
-The CLI command used to generate the contracts: `$ go run ./cmd import-contracts -cv v12.1.0 -alias aggchain-multisig -node 22`
+The CLI command used to generate the contracts: `$ go run ./cmd import-contracts -cv v12.2.0 -alias aggchain-multisig -node 22`
--- contracts/aggchain-multisig/depositcontractbase/depositcontractbase.go
+++ contracts/aggchain-multisig/depositcontractbase/depositcontractbase.go
@@ -31,8 +31,8 @@ var (
 
 // DepositcontractbaseMetaData contains all meta data concerning the Depositcontractbase contract.
 var DepositcontractbaseMetaData = &bind.MetaData{
-	ABI: "[{\"inputs\":[],\"name\":\"MerkleTreeFull\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"NewDepositCountExceedsMax\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"NonZeroValueForUnusedFrontier\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"SubtreeFrontierMismatch\",\"type\":\"error\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"leafHash\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32[32]\",\"name\":\"smtProof\",\"type\":\"bytes32[32]\"},{\"internalType\":\"uint32\",\"name\":\"index\",\"type\":\"uint32\"}],\"name\":\"calculateRoot\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"pure\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"depositCount\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"getRoot\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"leafHash\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32[32]\",\"name\":\"smtProof\",\"type\":\"bytes32[32]\"},{\"internalType\":\"uint32\",\"name\":\"index\",\"type\":\"uint32\"},{\"internalType\":\"bytes32\",\"name\":\"root\",\"type\":\"bytes32\"}],\"name\":\"verifyMerkleProof\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"pure\",\"type\":\"function\"}]",
-	Bin: "0x6080604052348015600e575f5ffd5b506102dd8061001c5f395ff3fe608060405234801561000f575f5ffd5b506004361061004a575f3560e01c80632dfdf0b51461004e5780635ca1e1651461006a57806383f2440314610072578063fb57083414610085575b5f5ffd5b61005760205481565b6040519081526020015b60405180910390f35b6100576100a8565b6100576100803660046101f9565b610126565b610098610093366004610235565b6101b3565b6040519015158152602001610061565b6020545f90819081805b602081101561011d578083901c6001166001036100f6576100ef5f82602081106100de576100de61027a565b0154855f9182526020526040902090565b9350610106565b5f84815260208390526040902093505b5f82815260208390526040902091506001016100b2565b50919392505050565b5f83815b60208110156101aa57600163ffffffff8516821c811690036101765761016f85826020811061015b5761015b61027a565b6020020135835f9182526020526040902090565b91506101a2565b61019f8286836020811061018c5761018c61027a565b60200201355f9182526020526040902090565b91505b60010161012a565b50949350505050565b5f816101c0868686610126565b1495945050505050565b8061040081018310156101db575f5ffd5b92915050565b803563ffffffff811681146101f4575f5ffd5b919050565b5f5f5f610440848603121561020c575f5ffd5b8335925061021d85602086016101ca565b915061022c61042085016101e1565b90509250925092565b5f5f5f5f6104608587031215610249575f5ffd5b8435935061025a86602087016101ca565b925061026961042086016101e1565b939692955092936104400135925050565b7f4e487b71000000000000000000000000000000000000000000000000000000005f52603260045260245ffdfea2646970667358221220e9da16864ff0fc6e8610b2f44ad40212c28238a80602f2ea74f8e5450b778d8564736f6c634300081c0033",
+	ABI: "[{\"inputs\":[],\"name\":\"MerkleTreeFull\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"NewDepositCountExceedsMax\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"NonZeroValueForUnusedFrontier\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"SubtreeFrontierMismatch\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"depositCount\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"getRoot\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
+	Bin: "0x6080604052348015600e575f5ffd5b506101298061001c5f395ff3fe6080604052348015600e575f5ffd5b50600436106030575f3560e01c80632dfdf0b51460345780635ca1e16514604e575b5f5ffd5b603c60205481565b60405190815260200160405180910390f35b603c6020545f90819081805b602081101560bd578083901c60011660010360975760915f8260208110608057608060c6565b0154855f9182526020526040902090565b935060a7565b5f84815260208390526040902093505b5f8281526020839052604090209150600101605a565b50919392505050565b7f4e487b71000000000000000000000000000000000000000000000000000000005f52603260045260245ffdfea264697066735822122034305dfe5b2d3603356536debdcedcb1c6b14af1c58db4f4127e90f6b101521264736f6c634300081c0033",
 }
 
 // DepositcontractbaseABI is the input ABI used to generate the binding from.
@@ -202,37 +202,6 @@ func (_Depositcontractbase *DepositcontractbaseTransactorRaw) Transact(opts *bin
 	return _Depositcontractbase.Contract.contract.Transact(opts, method, params...)
 }
 
-// CalculateRoot is a free data retrieval call binding the contract method 0x83f24403.
-//
-// Solidity: function calculateRoot(bytes32 leafHash, bytes32[32] smtProof, uint32 index) pure returns(bytes32)
-func (_Depositcontractbase *DepositcontractbaseCaller) CalculateRoot(opts *bind.CallOpts, leafHash [32]byte, smtProof [32][32]byte, index uint32) ([32]byte, error) {
-	var out []interface{}
-	err := _Depositcontractbase.contract.Call(opts, &out, "calculateRoot", leafHash, smtProof, index)
-
-	if err != nil {
-		return *new([32]byte), err
-	}
-
-	out0 := *abi.ConvertType(out[0], new([32]byte)).(*[32]byte)
-
-	return out0, err
-
-}
-
-// CalculateRoot is a free data retrieval call binding the contract method 0x83f24403.
-//
-// Solidity: function calculateRoot(bytes32 leafHash, bytes32[32] smtProof, uint32 index) pure returns(bytes32)
-func (_Depositcontractbase *DepositcontractbaseSession) CalculateRoot(leafHash [32]byte, smtProof [32][32]byte, index uint32) ([32]byte, error) {
-	return _Depositcontractbase.Contract.CalculateRoot(&_Depositcontractbase.CallOpts, leafHash, smtProof, index)
-}
-
-// CalculateRoot is a free data retrieval call binding the contract method 0x83f24403.
-//
-// Solidity: function calculateRoot(bytes32 leafHash, bytes32[32] smtProof, uint32 index) pure returns(bytes32)
-func (_Depositcontractbase *DepositcontractbaseCallerSession) CalculateRoot(leafHash [32]byte, smtProof [32][32]byte, index uint32) ([32]byte, error) {
-	return _Depositcontractbase.Contract.CalculateRoot(&_Depositcontractbase.CallOpts, leafHash, smtProof, index)
-}
-
 // DepositCount is a free data retrieval call binding the contract method 0x2dfdf0b5.
 //
 // Solidity: function depositCount() view returns(uint256)
@@ -293,35 +262,4 @@ func (_Depositcontractbase *DepositcontractbaseSession) GetRoot() ([32]byte, err
 // Solidity: function getRoot() view returns(bytes32)
 func (_Depositcontractbase *DepositcontractbaseCallerSession) GetRoot() ([32]byte, error) {
 	return _Depositcontractbase.Contract.GetRoot(&_Depositcontractbase.CallOpts)
-}
-
-// VerifyMerkleProof is a free data retrieval call binding the contract method 0xfb570834.
-//
-// Solidity: function verifyMerkleProof(bytes32 leafHash, bytes32[32] smtProof, uint32 index, bytes32 root) pure returns(bool)
-func (_Depositcontractbase *DepositcontractbaseCaller) VerifyMerkleProof(opts *bind.CallOpts, leafHash [32]byte, smtProof [32][32]byte, index uint32, root [32]byte) (bool, error) {
-	var out []interface{}
-	err := _Depositcontractbase.contract.Call(opts, &out, "verifyMerkleProof", leafHash, smtProof, index, root)
-
-	if err != nil {
-		return *new(bool), err
-	}
-
-	out0 := *abi.ConvertType(out[0], new(bool)).(*bool)
-
-	return out0, err
-
-}
-
-// VerifyMerkleProof is a free data retrieval call binding the contract method 0xfb570834.
-//
-// Solidity: function verifyMerkleProof(bytes32 leafHash, bytes32[32] smtProof, uint32 index, bytes32 root) pure returns(bool)
-func (_Depositcontractbase *DepositcontractbaseSession) VerifyMerkleProof(leafHash [32]byte, smtProof [32][32]byte, index uint32, root [32]byte) (bool, error) {
-	return _Depositcontractbase.Contract.VerifyMerkleProof(&_Depositcontractbase.CallOpts, leafHash, smtProof, index, root)
-}
-
-// VerifyMerkleProof is a free data retrieval call binding the contract method 0xfb570834.
-//
-// Solidity: function verifyMerkleProof(bytes32 leafHash, bytes32[32] smtProof, uint32 index, bytes32 root) pure returns(bool)
-func (_Depositcontractbase *DepositcontractbaseCallerSession) VerifyMerkleProof(leafHash [32]byte, smtProof [32][32]byte, index uint32, root [32]byte) (bool, error) {
-	return _Depositcontractbase.Contract.VerifyMerkleProof(&_Depositcontractbase.CallOpts, leafHash, smtProof, index, root)
 }
